4165175824f1ae545bb4e5bb942d7475747f8012
charlesslim <143763857+charlesslim@users.noreply.github.com>
2024-02-14 16:55:33 +0700

    Loaders

--- Loaders.lua
+++ Loaders.lua
@@ -34,7 +34,7 @@ background.Name = "Background"
 background.Parent = frame
 background.BackgroundTransparency = 1
 background.Size = UDim2.new(1, 0, 1, 0)
-background.Image = "https://roblox.com/asset/?id=1754142585"
+background.Image = "https://www.roblox.com/asset/?id=1754142585"
 
 local title = Instance.new("TextLabel")
 title.Name = "Title"
